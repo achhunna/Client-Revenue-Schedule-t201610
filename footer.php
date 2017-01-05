8ebd7da1e83a3ef139a7f27101b10d2c4c9386ae
achhunna <achhunna@gmail.com>
2017-01-05 10:40:22 -0800

    Year update

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<div class="footer">&copy; 2016 Achhunna. All rights reserved.</div>
+<div class="footer">&copy; <script type="text/javascript"> var date = new Date(); document.write( date.getFullYear() ); </script> Achhunna. All rights reserved.</div>
 </div>
 </body>
 </html>
